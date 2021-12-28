7053d460954148c82684345bcd944b5b6d2c374f
Danhickstein <danhickstein@gmail.com>
2021-12-28 11:25:54 -0700

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -21,7 +21,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'scipy.interpolate', 'scipy.integrate',
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'scipy.constants', 'scipy.interpolate', 'scipy.integrate',
                 'scipy.special', 'scipy.ndimage', 'scipy.fftpack']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
